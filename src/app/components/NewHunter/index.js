3df46b567dedb27395e62db9900b66b3931d7099
lvvst06 <lvvst06@gmail.com>
2025-04-02 21:54:32 +0700

    Add Header

---
+++ src/app/components/NewHunter/index.js
@@ -0,0 +1,13 @@
+export default function NewHunter() {
+  return (
+    <section className="new-hunter max-w-full">
+      <img
+        src="/newhunter.webp"
+        className="max-w-full"
+        alt="new hunter"
+        height={68}
+        width="100%"
+      />
+    </section>
+  );
+}
